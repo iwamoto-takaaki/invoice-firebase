86e675084712e02c4c357e2fe24dfff06a05452a
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-06-09 17:14:33 +0900

    add header compornent

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template lang="pug">
   #app
     #nav
-      router-link(to="/") Home
-      |  | 
-      router-link(to="/about") About
+      header-view/
     router-view/
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import HeaderView from '@/components/Header.vue';
+
+@Component({
+  components: {
+    HeaderView,
+  },
+})
+export default class App extends Vue {
+}
+</script>
 
 <style lang="sass">
 #app
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    #header-section
+        .title
+            h1 請求書作成ツール
+        .links
+            router-link(to="/") Home
+            |  | 
+            router-link(to="/about") About
+        .login
+            .signin-btn Sign In
+            .signout-btn Sign out
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class HeaderView extends Vue {
+}
+</script>
