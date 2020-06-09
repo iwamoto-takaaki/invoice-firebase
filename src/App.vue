12e5d0774f716fc383c07b0bc46392883d765b70
Takaaki Iwamoto <iwamoto.takaaki@gmail.com>
2020-06-09 16:19:03 +0900

    refactor

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,10 @@
 #nav
   padding: 30px
 
-#nav a 
-  font-weight: bold
-  color: #2c3e50
+  a 
+    font-weight: bold
+    color: #2c3e50
 
-#nav a.router-link-exact-active 
-  color: #42b983
+    &.router-link-exact-active 
+      color: #42b983
 </style>
